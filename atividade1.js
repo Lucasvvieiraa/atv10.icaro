67a837bdc619b5db59a33d7e9bfbfb4d0eb468c4
Lucasvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-04-15 08:39:12 -0300

    atv.js

---
+++ atividade1.js
@@ -0,0 +1,20 @@
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+
+    while (continuar) {
+        let numero = parseFloat(prompt("Digite um numero(ou digite '0'):"));
+
+
+        if (!isNaN(numero)) {
+            soma += numero;
+        } else {
+            alert("Por favor, digite um numero valido.");
+        }
+
+        continuar = confirm("deseja adicionar mais numeros?");
+    }
+
+    alert("A soma dos numeros é: " + soma);
+}
+somarNumeros();
