095c257cbabc7316ee8fab93e81e2075b97e4314
Lucasvvieiraa <lucas_vieira9@estudante.sesisenai.org.br>
2024-04-17 09:10:50 -0300

    finalizado

--- atividade4.js
+++ atividade4.js
@@ -1,14 +1,14 @@
 function ordemDecrescente() {
-    let numeros = {};
-    let contador=0;
+    let numeros = [];
 
-    for (contador = 0; contador < 4; contador++) (
-        numeros.push(parseint(prompt("Informe o" + parseint(contador+1) + "numero:")))
-    )
+    for (let i = 0; i < 4; i++) {
+        let num = parseInt(prompt('Digite um numero:'))       
+        numeros.push(num);
+    }
     numeros.sort(
         function(a,b) {
-            return b+a
+            return b-a
         }
     )
-    console.log("Números em ordem decrescente:" + numeros(0) + "-" +numeros(1) + "-" + numeros(2) + "-")
+    alert("Números em ordem decrescente:" + numeros[0] + "-" +numeros[1] + "-" + numeros[2] + "-" + numeros[3])
 }
--- atividade7.js
+++ atividade7.js
@@ -1,3 +1,49 @@
-function picoleteria(){
-    
+function picoleteria() {
+
+    const sabor = prompt("Digite o sabor do picolé:");
+
+    let preco;
+
+    switch(sabor) {
+        case "chocolate":
+            preco = 1.50;
+            alert("O picolé de chocolate custa: " + preco);
+            break;
+        case "morango":
+            preco = 2.50;
+            alert("O picole de morango custa:" + preco);
+            break;
+        case "creme":
+            preco = 2.50;
+            alert("O picole de creme custa:" + preco);
+            break;
+        case "manga":
+            preco = 3.20;
+            alert("O picole de manga custa:" + preco);
+            break;
+        case "melancia":
+            preco = 3.40;
+            alert("O picole de melancia custa:" + preco);
+            break;
+        case "vanilla ice":
+            preco = 3.00;
+            alert("O picole de vanilla ice custa:" + preco);
+            break;
+        case "ceu azul":
+            preco = 3.60;
+            alert("O picole de ceu azul custa:" + preco);
+            break;
+        case "brownie":
+            preco = 4.00;
+            alert("O picole de brownie custa:" + preco);
+            break;
+        case "hawaiano":
+            preco = 5.00;
+            alert("O picole de hawaiano custa:" + preco);
+            break;
+        
+        default:
+            alert ("Esse sabor inexistente")
+
+    }
 }
